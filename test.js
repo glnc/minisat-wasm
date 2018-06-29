12f12888b28152c11be7b5bc11b871d24bd203eb
Christian Bettinger <c.bettinger@hochschule-trier.de>
2018-06-29 11:20:47 +0200

    Added test case including a DIMACS comment line

--- test.js
+++ test.js
@@ -16,6 +16,7 @@ Module.onRuntimeInitialized = () => {
 
 function run() {
 	assert("p cnf 3 2\n1 -3 0\n2 3 -1 0", "SATISFIABLE");
+	assert("c This is a comment line.\np cnf 3 2\n1 -3 0\n2 3 -1 0", "SATISFIABLE");
 	assert("p cnf 1 2\n1 0\n-1 0", "UNSATISFIABLE");
 	assert("Hello, world!", "");
 }
