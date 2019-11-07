23890a47efc53284a71630b670faa1d05f4affa0
Moritz Goertz <goertzm@hochschule-trier.de>
2019-11-07 11:19:53 +0100

    updated sources to be compileable with emcc added prerun.js

--- src/minisat/utils/System.cc
+++ src/minisat/utils/System.cc
@@ -23,7 +23,7 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 
 #include "minisat/utils/System.h"
 
-#if defined(__linux__)
+#if defined(__linux__) || defined(__EMSCRIPTEN__)
 
 #include <stdlib.h>
 
